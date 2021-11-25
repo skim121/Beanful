21d975e2f5cd403a5c48409daaf77fd3dd5b1858
skim121 <skim29121@gmail.com>
2021-11-24 23:29:24 -0600

    Working bars reducing at set intervals and buttons to counteract

--- js/app.js
+++ js/app.js
@@ -1,37 +1,85 @@
 // Project One 
 
-// Click Play 
+// Character Building
+class Bean {
+    constructor(name, water, bug, love){
+        this.name = name; 
+        this.water = water; 
+        this.bug = bug;
+        this.love = love; 
+    } 
 
-
-
-
-
-   
-
-
-let testBean = {
-    name: "Bean",
-    age: 0,
-    water: 0,
-    bug: 0, 
-    love: 0, 
 }
 
-setTimeout(() => {
-    const progress = document.querySelector(".waterprogress");
-    progress.style.width = progress.getAttribute("data-done")+ "%";
-    progress.style.opacity = 1; 
-})
+const testBean = new Bean("",80,80,80);
 
+
+// Variables (including Jquery)
+
+const $waterBar = $(".waterprogress");
+const $bugBar = $(".bugprogress");
+const $loveBar = $(".loveprogress"); 
+const $waterButton = $(".waterbutton");
+
+
+function barReduce(){
+    setInterval(function(){
+        testBean.water -= 3;
+        $waterBar.css({width:`${testBean.water}%`});
+    },200);    
+    setInterval(function(){
+        testBean.bug -= 10;
+        $bugBar.css({width:`${testBean.bug}%`});
+    },700);    
+    setInterval(function(){
+        testBean.love -= 5;
+        $loveBar.css({width:`${testBean.love}%`});
+    },400);        
+}
+
+
+
+$(".waterbutton").click(function(){
+    if(testBean.water<90){
+        testBean.water += 10;
+        $waterBar.css({width:`${testBean.water}%`});
+    } else{
+        testBean.water = 100;
+        $waterBar.css({width:`${testBean.water}%`});
+    };
+});
+
+$(".bugbutton").click(function(){
+    if(testBean.bug<93){
+        testBean.bug += 7;
+        $bugBar.css({width:`${testBean.bug}%`});
+    } else{
+        testBean.bug = 100;
+        $bugBar.css({width:`${testBean.bug}%`});
+    };
+});
+
+$(".lovebutton").click(function(){
+    if(testBean.love<85){
+        testBean.love += 15;
+        $loveBar.css({width:`${testBean.love}%`});
+    } else{
+        testBean.love = 100;
+        $loveBar.css({width:`${testBean.love}%`});
+    };
+});
 
 
 $("#playbutton").click(function(){
     testBean.name = $("#nameinput").val();
     $(".beanname").html(testBean.name);
     $(".start").hide();
+    $(".play").show();   
+    barReduce();
 });
 
 $(".homebutton").click(function(){
     $(".start").show();
     $(".play").hide();
+    location.reload();
 });
